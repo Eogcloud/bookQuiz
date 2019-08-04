699364f12402ff1d602643e5d0e7403b6cc0e6ff
I310538 <eamonn.o.broithe@sap.com>
2019-08-04 16:31:35 +0100

    Add Redux state managment

--- src/components/AuthorQuiz/AuthorQuiz.js
+++ src/components/AuthorQuiz/AuthorQuiz.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom'
+import { connect } from 'react-redux';
 
 import './AuthorQuiz.css';
 import '../../bootstrap.min.css';
@@ -9,19 +10,43 @@ import Turn from '../Turn';
 import Continue from '../Continue';
 import Footer from '../Footer';
 
+const state = {
+	turnData: getTurnData(authors),
+	highlight: ''
+};
 
-function AuthorQuiz({ turnData, highlight, onAnswerSelected }) {
-	return (
-		<div className="container-fluid">
-			<Hero />
-			<Turn {...turnData} highlight={highlight} onAnswerSelected={onAnswerSelected} />
-			<Continue />
-			<p>
-				<Link to="/add">Add an Author></Link>
-			</p>
-			<Footer />
-		</div>
-	);
+function mapStatetoProps(state) {
+	return {
+		turnData: state.turnData,
+		highlight: state.highlight,
+	}
 }
+
+function mapDispatchtoProps(dispatch) {
+	return {
+		onAnswerSelected: (answer) => {
+			dispatch({ type: 'ANSWER_SELECTED', answer });
+		},
+		onContinue: () => {
+			dispatch({ type: 'CONTINUE' });
+		}
+	}
+}
+
+let AuthorQuiz = connect(mapStateToProps, mapDispatchToProps)(
+	function () {
+		return (
+			<div className="container-fluid">
+				<Hero />
+				<Turn {...turnData} highlight={highlight} onAnswerSelected={onAnswerSelected} />
+				<Continue />
+				<p>
+					<Link to="/add">Add an Author></Link>
+				</p>
+				<Footer />
+			</div>
+		)
+	}
+)
 
 export default AuthorQuiz;
--- src/index.js
+++ src/index.js
@@ -1,20 +1,100 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Redux from 'redux';
+import ReactRedux from 'react-redux';
 import { BrowserRouter, Route } from 'react-router-dom';
+import { shuffle, sample } from 'underscore';
+
 import App from './components/App/App'
 import AddAuthorForm from './components/AddAuthorForm'
+
 import "./index.css";
 
-function render() {
-	ReactDOM.render(
+const authors = [
+	{
+		name: "Mark Twain",
+		imageUrl: "images/authors/MarkTwain.jpg",
+		imageSource: "Wikimedia Commons",
+		books: ["The Adventures of Huckleberry Finn", "Roughing it", "The Gilded Age", "A Connecticut Yankee in King Arthur’s Court"]
+	},
+	{
+		name: "Ernest Hemingway",
+		imageUrl: "images/authors/ErnestHemingway.jpg",
+		imageSource: "Wikimedia Commons",
+		books: ["A Farewell to Arms", "A Moveable Feast", "The Old Man and the Sea", "The Sun Also Rises"]
+	},
+	{
+		name: "Roald Dahl",
+		imageUrl: "images/authors/RoaldDahl.jpg",
+		imageSource: "Wikimedia Commons",
+		books: ["The BFG", "Matilda", "Danny the Champion of the world", "The Twits"]
+	},
+	{
+		name: "Stephen King",
+		imageUrl: "images/authors/StephenKing.jpg",
+		imageSource: "Wikimedia Commons",
+		books: ["The Dark Tower", "Pet Semetary", "It", "Misery"]
+	},
+	{
+		name: "Hunter S Thompson",
+		imageUrl: "images/authors/HunterSThompson.jpg",
+		imageSource: "Wikimedia Commons",
+		books: ["Hell's Angels: The Strange and Terrible Saga of the Outlaw Motorcycle Gangs", "Fear and Loathing in Las Vegas: A Savage Journey to the Heart of the American Dream", "The Rum Diary"]
+	},
+	{
+		name: "James Joyce",
+		imageUrl: "images/authors/JamesJoyce.jpg",
+		imageSource: "Wikimedia Commons",
+		books: ["Dubliners", "A Portrait of the Artist as a Young Man", "Exiles", "Ulysses"]
+	},
+	{
+		name: "J R R Tolkien",
+		imageUrl: "images/authors/JRRTolkien.jpg",
+		imageSource: "Wikimedia Commons",
+		books: ["The Hobbit", "The Adventures of Tom Bombadil", "The Road Goes Ever On", "Tree and Leaf"]
+	}
+];
+
+function getTurnData(authors) {
+	const allBooks = authors.reduce((p, c, i) => {
+		return p.concat(c.books);
+	}, []);
+	const fourRandombooks = shuffle(allBooks).slice(0, 4);
+	const answer = sample(fourRandombooks);
+
+	return {
+		books: fourRandombooks,
+		author: authors.find((author) =>
+			author.books.some((title) => title === answer))
+	}
+}
+
+function reducer(state = { authors, turnData=getTurndata(authors), highlight: '' }, action) {
+
+	switch (action.type) {
+		case 'ANSWER_SELECTED':
+			const isCorrect = state.turnData.author.books.some((book) => book === action.turnData);
+			Object.assign({}, state, { highlight: isCorrect ? 'correct' : 'wrong' });
+
+		case 'COINTINUE':
+			return Object.assign({}, state, { highlight: '', turnData=getTurndata(state.authors) });
+		default:
+			return state;
+	}
+}
+
+let store = Redux.createStore(reducer);
+
+ReactDOM.render(
+	<ReactRedux.provider store={store}>
 		<BrowserRouter>
 			<React.Fragment>
 				<Route exact path="/" component={App} />
 				<Route path="/add" component={AddAuthorForm} />
 			</React.Fragment>
-		</BrowserRouter>,
+		</BrowserRouter>
+	</ReactRedux.provider>,
+	document.getElementById("root"));
 
-		document.getElementById("root"));
-}
 
-render();
+
